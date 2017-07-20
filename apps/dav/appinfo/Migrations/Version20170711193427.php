3c94d0d531f33e6c1f918f52b96c1b4dce367f23
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-07-20 21:49:47 +0300

    Fixup reset of autoincrement for Postgres

--- apps/dav/appinfo/Migrations/Version20170711193427.php
+++ apps/dav/appinfo/Migrations/Version20170711193427.php
@@ -1,14 +1,23 @@
 <?php
 namespace OCA\DAV\Migrations;
 
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Type;
+use OCP\IDBConnection;
 use OCP\Migration\ISchemaMigration;
 
 /**
  * Updates column type from integer to bigint
  */
 class Version20170711193427 implements ISchemaMigration {
+
+	/** @var IDBConnection */
+	private $connection;
+	
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+	}
 
 	public function changeSchema(Schema $schema, array $options) {
 		$prefix = $options['tablePrefix'];
@@ -19,6 +28,13 @@ class Version20170711193427 implements ISchemaMigration {
 			$idColumn = $table->getColumn('id');
 			if ($idColumn){
 				$idColumn->setType(Type::getType(Type::BIGINT));
+				
+				// Fixup postgres autoincrement
+				if ($this->connection->getDatabasePlatform() instanceof PostgreSqlPlatform){
+					$default = sprintf("nextval('%s_%s_seq'::regclass)", $table->getName(), $idColumn->getName());
+					$idColumn->setDefault($default);
+				}
+				
 				$idColumn->setOptions(['length' => 20]);
 			}
 
